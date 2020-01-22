a5bb5a35bf679dd0e4b36a6da070574aa16a81a6
Riccardo Locci <r.locci@beetobit.com>
2020-01-22 20:49:31 +0100

    NFO#8: Added dropdown select and node type update

--- src/components/GraphBuilder.js
+++ src/components/GraphBuilder.js
@@ -1,61 +1,8 @@
 import React, { Component } from 'react';
 import { GraphView } from 'react-digraph';
-import { Button } from '@material-ui/core';
 
 import '../css/GraphBuilder.css';
 
-// const data = {
-//     nodes: [
-//       {
-//         id: 1,
-//         title: "Node A",
-//         x: Math.random() * 600, // "x": 258.3976135253906,
-//         y: Math.random() * 600,// "y": 331.9783248901367,
-//         type: "empty"
-//       },
-//       {
-//         id: 2,
-//         title: "Node B",
-//         x: Math.random() * 600, // "x": 593.9393920898438,
-//         y: Math.random() * 600,// "y": 260.6060791015625,
-//         type: "empty"
-//       },
-//       {
-//         id: 3,
-//         title: "Node C",
-//         x: Math.random() * 600, // "x": 237.5757598876953,
-//         y: Math.random() * 600,// "y": 61.81818389892578,
-//         type: "custom"
-//       },
-//       {
-//         id: 4,
-//         title: "Node C",
-//         x: Math.random() * 600, // "x": 600.5757598876953,
-//         y: Math.random() * 600,// "y": 600.81818389892578,
-//         type: "custom"
-//       }
-//     ],
-//     edges: [
-//       {
-//         source: 1,
-//         target: 2,
-//         type: "emptyEdge",
-//         handleText: '5; 5',
-//         cost: 3,
-//         flow: 2,
-//         capacity: 8
-//       },
-//       {
-//         source: 2,
-//         target: 4,
-//         type: "emptyEdge",
-//         handleText: '10; 5',
-//         cost: 5,
-//         flow: 4,
-//         capacity: 6
-//       }
-//     ]
-//   };
 
 const GraphConfig =  {
     NodeTypes: {
@@ -64,7 +11,23 @@ const GraphConfig =  {
             shapeId: "#empty", // relates to the type property of a node
             shape: (
                 <symbol viewBox="0 0 100 100" id="empty" key="0">
-                    <circle cx="50" cy="50" r="5" stroke="green" />
+                    <circle cx="50" cy="50" r="5" stroke="grey" />
+                </symbol>
+            )
+        },
+        start_node: {
+            shapeId: "#start_node", // relates to the type property of a node
+            shape: (
+                <symbol viewBox="0 0 100 100" id="start_node" key="0">
+                    <circle cx="50" cy="50" r="5" stroke="orange" strokeWidth="2"/>
+                </symbol>
+            )
+        },
+        end_node: {
+            shapeId: "#end_node", // relates to the type property of a node
+            shape: (
+                <symbol viewBox="0 0 100 100" id="end_node" key="0">
+                    <circle cx="50" cy="50" r="5" stroke="green" strokeWidth="2"/>
                 </symbol>
             )
         },
@@ -110,79 +73,78 @@ class GraphBuilder extends Component {
 
         const { labelSize, file } = this.props;
         
+        console.log('file', file)
+
         return(
             <div className="GraphBuilder-root">
-                <div className="GraphBuilder-graph" >
-                    <GraphView  ref='GraphView'
-                        nodeKey="id"
-                        nodes={file.nodes}
-                        edges={file.edges}
-                        selected={this.state.selected}
-                        nodeTypes={NodeTypes}
-                        nodeSubtypes={NodeSubtypes}
-                        edgeTypes={EdgeTypes}
-                        readOnly
-                        onSelectNode={() => {}}
-                        onCreateNode={() => {}}
-                        onUpdateNode={() => {}}
-                        onDeleteNode={() => {}}
-                        onSelectEdge={() => {}}
-                        onCreateEdge={() => {}}
-                        onSwapEdge={() => {}}
-                        onDeleteEdge={() => {}}
+                <GraphView  ref='GraphView'
+                    nodeKey="id"
+                    nodes={file.nodes}
+                    edges={file.edges}
+                    selected={this.state.selected}
+                    nodeTypes={NodeTypes}
+                    nodeSubtypes={NodeSubtypes}
+                    edgeTypes={EdgeTypes}
+                    readOnly
+                    onSelectNode={() => {}}
+                    onCreateNode={() => {}}
+                    onUpdateNode={() => {}}
+                    onDeleteNode={() => {}}
+                    onSelectEdge={() => {}}
+                    onCreateEdge={() => {}}
+                    onSwapEdge={() => {}}
+                    onDeleteEdge={() => {}}
 
-                        edgeArrowSize={5}
+                    edgeArrowSize={5}
 
-                        afterRenderEdge={(id, element, edge, edgeContainer, isEdgeSelected) => {
-                            /***** Setting edge color *****/ 
+                    afterRenderEdge={(id, element, edge, edgeContainer, isEdgeSelected) => {
+                        /***** Setting edge color *****/ 
 
-                            let comp = edgeContainer.querySelector('.edge');
-                            this.setAttribute(comp, 'style', 'stroke: black;', false);
+                        let comp = edgeContainer.querySelector('.edge');
+                        this.setAttribute(comp, 'style', 'stroke: black;', false);
 
-                            /***** Setting arrow end color *****/ 
+                        /***** Setting arrow end color *****/ 
 
-                            comp = document.querySelector('.arrow');
-                            this.setAttribute(comp, 'style', 'fill: black;', false);
+                        comp = document.querySelector('.arrow');
+                        this.setAttribute(comp, 'style', 'fill: black;', false);
 
-                            comp = edgeContainer.querySelector('.edge-text');
+                        comp = edgeContainer.querySelector('.edge-text');
 
-                            /***** Rotating labels according to edge slope *****/ 
+                        /***** Rotating labels according to edge slope *****/ 
 
-                            let {x: x1, y: y1} = element.props.sourceNode;
-                            let {x: x2, y: y2} = element.props.targetNode;
+                        let {x: x1, y: y1} = element.props.sourceNode;
+                        let {x: x2, y: y2} = element.props.targetNode;
 
-                            let tan = (y2 - y1)/(x2 - x1);
-                            let deg = Math.atan(tan)*180/Math.PI;;
+                        let tan = (y2 - y1)/(x2 - x1);
+                        let deg = Math.atan(tan)*180/Math.PI;;
 
-                            this.setAttribute(comp, 'style', `fill: black; stroke: white; stroke-width: 0.5px; font-weight: bold; font-size: ${labelSize}`, false);
-                            this.setAttribute(comp, 'transform', ` rotate(${deg}) translate(0, -${labelSize})`);
-                            comp.innerHTML = edge.flow ? edge.flow : '';
+                        this.setAttribute(comp, 'style', `fill: black; stroke: white; stroke-width: 0.5px; font-weight: bold; font-size: ${labelSize}`, false);
+                        this.setAttribute(comp, 'transform', ` rotate(${deg}) translate(0, -${labelSize})`);
+                        comp.innerHTML = edge.flow ? edge.flow : '';
 
-                            /***** Labels for costs and maximum capacity *****/ 
+                        /***** Labels for costs and maximum capacity *****/ 
+                        
+                        if(edge.cost){
+                            let costTag = comp.cloneNode(true);
                             
-                            if(edge.cost){
-                                let costTag = comp.cloneNode(true);
-                                
-                                this.setAttribute(costTag, 'transform', ` translate(-${edge.capacity ? labelSize : 0}, ${labelSize*2})`);
-                                costTag.style.fill = 'red';
-                                costTag.innerHTML = edge.cost;
-                                
-                                comp.parentElement.appendChild(costTag);
-                            }
+                            this.setAttribute(costTag, 'transform', ` translate(-${edge.capacity ? labelSize : 0}, ${labelSize*2})`);
+                            costTag.style.fill = 'red';
+                            costTag.innerHTML = edge.cost;
                             
-                            if(edge.capacity){
-                                let capacityTag = comp.cloneNode(true);
+                            comp.parentElement.appendChild(costTag);
+                        }
+                        
+                        if(edge.capacity){
+                            let capacityTag = comp.cloneNode(true);
 
-                                this.setAttribute(capacityTag, 'transform', ` translate(${edge.cost ? labelSize : 0}, ${labelSize*2})`);
-                                capacityTag.style.fill = 'blue';
-                                capacityTag.innerHTML = edge.capacity;
-                                
-                                comp.parentElement.appendChild(capacityTag);
-                            }
-                        }}
-                    />
-                </div>
-                <Button className="GraphBuilder-button" onClick={(e) => this.props.onReset(null)}>RESET</Button>
+                            this.setAttribute(capacityTag, 'transform', ` translate(${edge.cost ? labelSize : 0}, ${labelSize*2})`);
+                            capacityTag.style.fill = 'blue';
+                            capacityTag.innerHTML = edge.capacity;
+                            
+                            comp.parentElement.appendChild(capacityTag);
+                        }
+                    }}
+                />
             </div>
         ) 
     }
--- src/components/SPP.js
+++ src/components/SPP.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Dropzone from './Dropzone';
 import GraphBuilder from './GraphBuilder';
-import { Paper } from '@material-ui/core';
+import { Button, MenuItem, OutlinedInput, Paper, TextField } from '@material-ui/core';
 
 import '../css/SPP.css'
 
@@ -10,11 +10,30 @@ class SPP extends Component {
         super(props)
         this.state = {
             file: null,
-            message: ''
+            message: '',
+            start_node: '',
+            end_node: ''
         }
     }
 
     getFile = (file) => this.setState({ file });
+
+    onChange = (key) => (e) => {
+        var { file } = this.state;
+
+        for(let n of file.nodes){
+            if(n.id === e.target.value.id){
+                n.type = key
+                n.title = 'START'
+            }
+            else if(n.type === key) n.type = 'empty';
+        }
+
+        this.setState({
+            [key]: e.target.value,
+            file
+        })
+    }
 
     showMessage = (message) => {
         this.setState({ message });
@@ -45,11 +64,46 @@ class SPP extends Component {
                     </Paper>
                 </div>}
                 
-                {file && <GraphBuilder
-                    labelSize = {13}
-                    file={file}
-                    onReset={this.getFile}
-                />}
+                {file && <div className="SPP-graph">
+                    <GraphBuilder
+                        labelSize = {13}
+                        file={file}
+                    />
+                    
+                    <div className="SPP-spacer"><Button className="SPP-button" onClick={(e) => this.setState({file: null, start_node: '', end_node: ''})}>RESET</Button></div>
+                    
+                    <div className="SPP-spacer">
+                        <TextField
+                            select
+                            className="SPP-select"
+                            label="Start Node"
+                            value={this.state.start_node}
+                            onChange={this.onChange('start_node')}
+                            margin="normal"
+                            input={<OutlinedInput/>}
+                        >
+                            <MenuItem key={'empty'} value='' />
+                            {file.nodes.map(e => (
+                                <MenuItem key={e.id} value={e}>{e.title}</MenuItem>
+                            ))}
+                        </TextField>
+
+                        <TextField
+                            select
+                            className="SPP-select"
+                            label="End Node"
+                            value={this.state.end_node}
+                            onChange={this.onChange('end_node')}
+                            margin="normal"
+                            input={<OutlinedInput/>}
+                        >
+                            <MenuItem key={'empty'} value='' />
+                            {file.nodes.map(e => (
+                                <MenuItem key={e.id} value={e}>{e.title}</MenuItem>
+                            ))}
+                        </TextField>
+                    </div>
+                </div>}
             </div>
         )
     }
