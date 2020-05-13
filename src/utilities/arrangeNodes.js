b7c46908bc27da681916f1b52216e7af4e9fa7bb
Riccardo Locci <riccardo.locci.95@gmail.com>
2020-05-13 22:57:23 +0200

    NFO#10: First prototype

--- src/utilities/arrangeNodes.js
+++ src/utilities/arrangeNodes.js
@@ -1,4 +1,31 @@
-export default (f) => {
+let forceArrange = (f) => {
+    const d3 = require('d3');
+    let { nodes, edges } = f;
+
+    let sim = d3.forceSimulation(nodes)
+        .force("charge", d3.forceManyBody().strength(-200))
+        .force("link", d3.forceLink(JSON.parse(JSON.stringify(edges)))
+            .id(n => n.id).distance(e => e.cost ? e.cost*50 : 70));
+
+    sim.tick([15])
+
+    f.nodes = nodes.map( n => ({
+        id: n.id,
+        title: n.title,
+        x: n.x,
+        y: n.y,
+        type: 'empty'
+    }));
+
+    for(let el of edges){
+        el.type = 'emptyEdge';
+        el.handleText = '.';
+    }
+
+    return f;
+}
+
+let randomArrange = (f) => {
     for(let el of f.nodes){
         el.x = Math.random() * 600;
         el.y = Math.random() * 600;
@@ -11,4 +38,15 @@ export default (f) => {
     }
 
     return f;
+}
+
+export default (f, mode = 'force') => {
+    switch(mode){
+        case 'force':
+            return forceArrange(f);
+        case 'random':
+            return randomArrange(f);
+        default:
+            return f;
+    }
 }
